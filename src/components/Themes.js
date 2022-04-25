79b88fe44f389b2574b1d92eeecaae25430d26f9
gopi <gopigaurav9@gmail.com>
2022-04-25 12:11:23 +0530

    first commit

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -4,7 +4,7 @@ export const lightTheme = {
 
     body:"#FCF6F4",
     text:"#000000",
-    fontFamily:"'Source Sans Pro', sans-serif",
+    fontFamily:"'Josefin Sans', sans-serif",
     bodyRgba : "252, 246, 244",
     textRgba:"0,0,0",
 }
@@ -13,7 +13,7 @@ export const DarkTheme = {
 
     body:"#000000",
     text:"#FCF6F4",
-    fontFamily:"'Source Sans Pro', sans-serif",
+    fontFamily:"'Josefin Sans', sans-serif",
     textRgba : "252, 246, 244",
     bodyRgba:"0,0,0",
 }
