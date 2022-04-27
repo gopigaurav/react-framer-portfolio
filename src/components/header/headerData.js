19998d0872ff83f0bbbd35e6b8e3d777beceecb9
gopi <gopigaurav9@gmail.com>
2022-04-27 18:59:41 +0530

    first commit

---
+++ src/components/header/headerData.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      path: "home",
+      label: "Home",
+    },
+    {
+      path: "about",
+      label: "About",
+    },
+    {
+      path: "skillSet",
+      label: "Skill Set",
+    },
+    {
+      path: "timeLine",
+      label: "Time Line",
+    },
+    {
+      path: "projects",
+      label: "Projects",
+    },
+  ];
+  
