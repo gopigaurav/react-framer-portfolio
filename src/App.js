178e7268c4c56888753e5807b39187dc229cef46
gopi <gopigaurav9@gmail.com>
2022-04-30 23:17:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch, useLocation } from "react-router";
-import React, { useState} from "react"
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, DarkTheme } from "./components/Themes";
 import GlobalStyle from "./globalStyles";
@@ -13,28 +13,44 @@ import WorkPage from "./components/WorkPage";
 import MySkillsPage from "./components/MySkillsPage";
 import { AnimatePresence } from "framer-motion";
 import SoundBar from "./subComponents/SoundBar";
+import Sticky from "react-stickynode";
+import Header from "./components/header/header";
 
 function App() {
   const location = useLocation();
   const [click, setClick] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleStateChange = (status) => {
+    if (status.status === Sticky.STATUS_FIXED) {
+      setIsSticky(true);
+    } else if (status.status === Sticky.STATUS_ORIGINAL) {
+      setIsSticky(false);
+    }
+  };
 
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={DarkTheme}>
-        <SoundBar />
-        <LogoComponent theme={DarkTheme ? "dark" : "light"} />
-        <SocialIcons theme={click ? "dark" : "light"} />
-        {/* For framer-motion animation on page change! */}
-        <AnimatePresence exitBeforeEnter>
-          <Switch location={location} key={location.pathname}>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/about" component={AboutPage} />
-            {/*<Route exact path="/blog" component={BlogPage} />*/}
-            <Route exact path="/work" component={WorkPage} />
-            <Route exact path="/skills" component={MySkillsPage} />
-          </Switch>
-        </AnimatePresence>
+      <ThemeProvider theme={lightTheme}>
+        <div>
+          <SoundBar />
+          <Sticky innerZ={1001} top={0} onStateChange={handleStateChange}>
+            <Header className={`${isSticky ? "sticky" : "unSticky"}`} />
+          </Sticky>
+          <LogoComponent theme={DarkTheme ? "dark" : "light"} />
+          <SocialIcons theme={click ? "dark" : "light"} />
+          {/* For framer-motion animation on page change! */}
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/about" component={AboutPage} />
+              {/*<Route exact path="/blog" component={BlogPage} />*/}
+              <Route exact path="/work" component={WorkPage} />
+              <Route exact path="/skills" component={MySkillsPage} />
+            </Switch>
+          </AnimatePresence>
+        </div>
       </ThemeProvider>
     </>
   );
